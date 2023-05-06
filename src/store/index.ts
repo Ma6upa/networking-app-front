6a965da7cb6ff1b302ced0ed16c5e2be5f247027
Ma6upa <vaud55@gmail.com>
2023-05-07 02:20:02 +0400

    add default typed redux logic and plug it in to app

---
+++ src/store/index.ts
@@ -0,0 +1,10 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import thunk from "redux-thunk";
+import { rootReducer } from "./reducers";
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: [
+    thunk
+  ],
+})
