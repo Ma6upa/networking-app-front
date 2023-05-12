5d19a3c88c03e15507bfa9cb5324e9bf515f7ad4
Ma6upa <vaud55@gmail.com>
2023-05-12 23:33:18 +0400

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,56 @@
 import React from 'react';
-import { Route, Routes } from "react-router-dom";
+import { useEffect } from 'react'
+import { Link, Route, Routes } from "react-router-dom";
+import {
+  AppBar,
+  Toolbar,
+  Typography
+} from "@mui/material"
 import { AuthPage } from './pages/AuthPage';
 import { RegistrationPage } from './pages/RegistrationPage';
 import { UserPage } from './pages/UserPage';
+import FeedPage from './pages/FeedPage';
+import FriendsPage from './pages/FriendsPage';
+import UsersPage from './pages/UsersPage';
+import { useActions } from './hooks/useActions';
+import { useTypedSelector } from './hooks/useTypedSelector';
 
 function App() {
+  const { user } = useTypedSelector(state => state.user);
+
   return (
     <>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <Link to={'/user/'+user.id} style={{ textDecoration: 'none', color: 'white' }}>
+            <Typography component="h2" variant="h6">
+              Моя страница
+            </Typography>
+          </Link>
+          <Link to="/feed" style={{ textDecoration: 'none', color: 'white', marginLeft: 20 }}>
+            <Typography component="h2" variant="h6">
+              Лента
+            </Typography>
+          </Link>
+          <Link to="/friends" style={{ textDecoration: 'none', color: 'white', marginLeft: 20 }}>
+            <Typography component="h2" variant="h6">
+              Друзья
+            </Typography>
+          </Link>
+          <Link to="/users" style={{ textDecoration: 'none', color: 'white', marginLeft: 20 }}>
+            <Typography component="h2" variant="h6">
+              Пользователи
+            </Typography>
+          </Link>
+        </Toolbar>
+      </AppBar>
       <Routes>
         <Route path="/" element={<AuthPage />} />
         <Route path="/registration" element={<RegistrationPage />} />
         <Route path="/user/:id" element={<UserPage />} />
+        <Route path="/feed" element={<FeedPage />} />
+        <Route path="/friends/" element={<FriendsPage />} />
+        <Route path="/users" element={<UsersPage />} />
       </Routes>
     </>
   );
